b75658f387dfc60affe304a54f04e15bd27275c8
SeanDMeehan <sean@themeehans.net>
2026-08-14 09:51:27 -0500

    added correct images and links

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -2,6 +2,8 @@ import type { Project } from "../types/portfolio";
 import satelliteML from "../assets/images/SatelliteML.png";
 import natTwentyOne from "../assets/images/NatTwentyOne.png";
 import linAlgLib from "../assets/images/LinAlgLib.png";
+import SatelliteOrbitalPath from "../assets/papers/SatelliteOrbitalPath.pdf";
+import Website from "../assets/images/Website.png";
 
 export const projects: Project[] = [
     {
@@ -10,7 +12,7 @@ export const projects: Project[] = [
         summary: "An end to end Data Science project using a Physics-Informed Neural Network trained on satellite and solar weather data to reduce the error in satellite orbital path propogations made using the method of simplified general perturbations. The project began with the automated aquasition of NASA and CelesTrack data. It continued to data cleaning, exploratory data analysis, the merging of the datasets, feature engineering, and modeling. The result was a hybrid Gated Recurrent Unit - Multi Layer Perceptron machine learning algorithm capable of reducing the average error in the predicted position of a satellite by 49%.",
         technologies: ["Python", "Pandas", "Numpy", "Matplotlib", "PyTorch", "BeautifulSoup", "sgp4", "requests"],
         links: [
-            { label: "Github", href: "#" }
+            { label: "PDF", href: SatelliteOrbitalPath }
         ],
         image: satelliteML,
         featured: true
@@ -21,7 +23,7 @@ export const projects: Project[] = [
         summary: "A React and TypeScript DnD application for customizable character and story creation and management. The app utilizes IndexedDB via Dexie to manage user and game data, a custom built database interface and state management system, and several libraries -- namely ReactFlow, React-Grid-Layout, and Material UI. The result is widget based character management and graph based story management systems that allow the user to create and interact with their own characters and their own stories in an individualized environment through a seemless, aesthetic, and intuitive User Interface.",
         technologies: ["React", "TypeScript", "Vite", "Dexie", "IndexedDB", "Python", "Pandas", "Ollama"],
         links: [
-            { label: "GitHub", href: "#" }
+            { label: "GitHub", href: "https://github.com/jthunt1515/NatTwentyOne-2.0" }
         ],
         image: natTwentyOne,
         featured: true
@@ -45,7 +47,7 @@ export const projects: Project[] = [
         links: [
             { label: "GitHub", href: "https://github.com/SeanDMeehan/SeanDMeehan.github.io"}
         ],
-        image: natTwentyOne,
+        image: Website,
         featured: false
     },
     
